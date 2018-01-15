a0b8b23a4810322b3ec53c71c741e16d578d6ded
荆博 <525456448@qq.com>
2018-01-15 14:50:13 +0800

    MOD：ActivityIndicator优化demo显示

--- demo/app/src/ui/ActivityIndicator/index.js
+++ demo/app/src/ui/ActivityIndicator/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { ScrollView, View, Text } from 'react-native'
 import { Style } from '../../../common'
-import { ActivityIndicator } from 'fego-rn'
+import { ActivityIndicator, List } from 'fego-rn'
+const ListItem = List.ListItem;
+
 export default class Page extends Component {
 	static navigationOptions = {
 		title: '活动指示器'
 	}
-	constructor(props){
+	constructor(props) {
 		super(props)
 		this.state = {
 			toast1Visible: false,
@@ -16,11 +18,33 @@ export default class Page extends Component {
 	render() {
 		return (
 			<ScrollView style={Style.container}>
-				<ActivityIndicator />
-				<ActivityIndicator color="#33F" textStyle={{color: '#33F'}} text="正在加载" />
-				<ActivityIndicator size="large" />
-				<ActivityIndicator text="正在加载" />
-				<ActivityIndicator visible={this.state.toast1Visible} hasToast={true} size="large" color="#FFF"/>
+				<Text style={Style.title}>ActivityIndicator</Text>
+				<List>
+					<ListItem>
+						<Text>默认：</Text>
+						<ActivityIndicator />
+					</ListItem>
+					<ListItem>
+						<Text>size设为large：</Text>
+						<ActivityIndicator size="large" />
+					</ListItem>
+					<ListItem>
+						<Text>text设为正在加载：</Text>
+						<ActivityIndicator text="正在加载" />
+					</ListItem>
+					<ListItem>
+						<Text>变更颜色：</Text>
+						<ActivityIndicator color="#33F" textStyle={{ color: '#33F' }} text="正在加载" />
+					</ListItem>
+					<ListItem>
+						<Text>visible为false</Text>
+						<ActivityIndicator visible={this.state.toast1Visible} hasToast={true} size="large" color="#FFF" />
+					</ListItem>
+				</List>
+				
+
+				
+				
 			</ScrollView>
 		);
 	}
