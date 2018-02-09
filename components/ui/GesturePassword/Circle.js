bb7c2d9770ec009973684a03ab1fa5b1b582ee56
沈文兴 <bjshenwenxing@corp.netease.com>
2018-02-09 18:29:45 +0800

    MOD：格式化switch

--- components/ui/GesturePassword/Circle.js
+++ components/ui/GesturePassword/Circle.js
@@ -1,67 +1,80 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { StyleSheet, View } from 'react-native'
+import React, {
+  Component,
+} from 'react';
+import PropTypes from 'prop-types';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
 
 const styles = StyleSheet.create({
-	container: {
-		position: 'absolute',
-	}
-})
+  container: {
+    position: 'absolute',
+  },
+});
 
-//const borderWidth = 1
+// const borderWidth = 1
 
 export default class Circle extends Component {
+  static defaultProps = {
+    isFill: false,
+    backgroundColor: 'transparent',
+  }
 
-	static defaultProps = {
-		isFill: false,
-		backgroundColor: 'transparent',
-	}
+  static propTypes = {
+    isFill: PropTypes.bool,
+    color: PropTypes.string.isRequired,
+    radius: PropTypes.number.isRequired,
+    borderWidth: PropTypes.number.isRequired,
+    backgroundColor: PropTypes.string,
+    position: PropTypes.shape({
+      left: PropTypes.number.isRequired,
+      top: PropTypes.number.isRequired,
+    }).isRequired,
 
-	static propTypes = {
-		isFill: PropTypes.bool,
-		color: PropTypes.string.isRequired,
-		radius: PropTypes.number.isRequired,
-		borderWidth: PropTypes.number.isRequired,
-		backgroundColor: PropTypes.string,
-		position: PropTypes.shape({
-			left: PropTypes.number.isRequired,
-			top: PropTypes.number.isRequired,
-		}).isRequired,
+  }
 
-	}
+  // 构造
+  constructor(props) {
+    super(props);
+    // 初始状态
+    this.state = {};
+    this._diameter = props.radius * 2;
+  }
 
-	// 构造
-	constructor(props) {
-		super(props)
-		// 初始状态
-		this.state = {}
-		this._diameter = props.radius * 2
-	}
-
-	render() {
-		let {isFill,color,borderWidth,backgroundColor,radius,position} = this.props;
-		return (
-			<View
-				//onLayout={ (e) => {
-				//}}
-				style={[
-					styles.container,
-					isFill ?
-						{ backgroundColor: color, } :
-						{ borderColor: color, borderWidth:borderWidth, backgroundColor: backgroundColor },
-					{
-						width: this._diameter,
-						height: this._diameter,
-						borderRadius: radius,
-						//left: this.props.position.left - borderWidth,
-						//top: this.props.position.top - borderWidth,
-						left: position.left,
-						top: position.top,
-					}
-				]}>
-				{this.props.children}
-			</View>
-		)
-	}
-
+  render() {
+    const {
+      isFill,
+      color,
+      borderWidth,
+      backgroundColor,
+      radius,
+      position,
+    } = this.props;
+    return (
+      <View
+      // onLayout={ (e) => {
+      // }}
+        style={[
+          styles.container,
+          isFill ? { backgroundColor: color } : {
+            borderColor: color,
+            borderWidth,
+            backgroundColor,
+          },
+          {
+            width: this._diameter,
+            height: this._diameter,
+            borderRadius: radius,
+            // left: this.props.position.left - borderWidth,
+            // top: this.props.position.top - borderWidth,
+            left: position.left,
+            top: position.top,
+          },
+        ]}
+      >
+        { this.props.children }
+      </View>
+    );
+  }
 }
