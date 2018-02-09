40191228bfaa3665f6f6cd61718f6d1802b38ac6
史晓霞 <dx910114@126.com>
2018-02-09 19:23:38 +0800

    MOD：测试文件格式化

--- components/ui/Icon/__test__/index.test.js
+++ components/ui/Icon/__test__/index.test.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import { Text } from 'react-native'
-import Renderer from 'react-test-renderer'
-import { shallow } from 'enzyme'
-import Icon from '../index'
+import React from 'react';
+import { Text } from 'react-native';
+import Renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import { describe, it, expect } from 'jest';
+import Icon from '../index';
 
 describe('Icon Tests', () => {
-	it('renders correctly', () => {
-		const tree = Renderer.create((
-			<Icon name='user' />
-		)).toJSON();
-		
-		expect(tree).toMatchSnapshot();
-	});
+  it('renders correctly', () => {
+    const tree = Renderer.create((
+      <Icon name="user" />
+    )).toJSON();
 
-	it('children test', () => {
-		const wrapper = shallow(
-			<Icon name='user'>
-				<Text>icon child</Text>
-			</Icon>
-		);
-		
-		expect(wrapper.contains(<Text>icon child</Text>)).toBe(true);
-		expect(wrapper.contains('icon child')).toBe(true);
-	});
-})
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('children test', () => {
+    const wrapper = shallow(<Icon name="user"><Text>icon child</Text></Icon>);
+    expect(wrapper.contains(<Text>icon child</Text>)).toBe(true);
+    expect(wrapper.contains('icon child')).toBe(true);
+  });
+});
--- components/ui/Modal/__test__/index.test.js
+++ components/ui/Modal/__test__/index.test.js
@@ -1,43 +1,44 @@
-import React from 'react'
-import Renderer from 'react-test-renderer'
-import { View, Text } from 'react-native'
-import { shallow } from 'enzyme'
-import Modal from '../index'
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import { View, Text } from 'react-native';
+import { shallow } from 'enzyme';
+import { describe, it, expect, jest, beforeEach } from 'jest';
+import Modal from '../index';
 
 describe('Modal Tests', () => {
-	let Comp, handler;
-	beforeEach(() => {
-		handler = jest.fn();
-		Comp = <Modal
-			visible={true}
-			animateAppear={false}
-			onClose={handler}
-		>
-			<View>
-				<Text>简单模态框</Text>
-			</View>
-		</Modal>
-	});
+  let Comp;
+  let handler;
+  beforeEach(() => {
+    handler = jest.fn();
+    Comp = (
+      <Modal
+        visible
+        animateAppear={false}
+        onClose={handler}
+      >
+        <View>
+          <Text>简单模态框</Text>
+        </View>
+      </Modal>
+    );
+  });
 
-	it('renders correctly', () => {
-		const tree = Renderer.create(Comp).toJSON();
-		expect(tree).toMatchSnapshot();
-	});
+  it('renders correctly', () => {
+    const tree = Renderer.create(Comp).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-	it('simple content test', () => {
-		const wrapper = shallow(Comp);
+  it('simple content test', () => {
+    const wrapper = shallow(Comp);
 
-		expect(wrapper.contains(<View>
-			<Text>简单模态框</Text>
-		</View>)).toBe(true);
-	});
+    expect(wrapper.contains(<View><Text>简单模态框</Text></View>)).toBe(true);
+  });
 
-	it('close modal', () => {
-		const wrapper = shallow(Comp);
+  it('close modal', () => {
+    const wrapper = shallow(Comp);
 
-		wrapper.childAt(0).childAt(0).simulate('press');
-		expect(handler).toHaveBeenCalled();
-	});
-})
-
+    wrapper.childAt(0).childAt(0).simulate('press');
+    expect(handler).toHaveBeenCalled();
+  });
+});
 
