70a266e18c71d77ed8a2a44dc79cb1c095c1a968
荆博 <525456448@qq.com>
2018-02-09 16:51:25 +0800

    MOD:修改格式

--- demo/app/src/ui/Slider/index.js
+++ demo/app/src/ui/Slider/index.js
@@ -1,39 +1,42 @@
-import React, { Component } from 'react'
-import { ScrollView, Text } from 'react-native'
-import { Style } from '../../../common'
-import { Slider,Toast } from 'fego-rn'
+import React, { Component } from 'react';
+import { ScrollView, Text } from 'react-native';
+import { Slider, Toast } from 'fego-rn';
+import { Style } from '../../../common';
 
 export default class Page extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			value: 10
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 10,
+    };
+  }
 
-	render() {
-		return (
-			<ScrollView style={Style.container}>
-				<Text style={Style.text}>Slider</Text>
+  render() {
+    return (
+      <ScrollView style={Style.container}>
+        <Text style={Style.text}>Slider</Text>
 
-				<Text style={Style.title}>受控组件，Value: {this.state.value}</Text>
-				<Slider value={this.state.value} onValueChange={(value) => {
-					if (value < 50) {
-						this.setState({ value })
-					}else{
-						Toast.info('大于50，禁止在增大');
-					}
-				}} />
+        <Text style={Style.title}>受控组件，Value: {this.state.value}</Text>
+        <Slider
+          value={this.state.value}
+          onValueChange={(value) => {
+            if (value < 50) {
+              this.setState({ value });
+            } else {
+              Toast.info('大于50，禁止在增大');
+            }
+          }}
+        />
 
-				<Text style={Style.title}>非受控组件，type = circle</Text>
-				<Slider type='circle' defaultValue={20} />
-				<Text style={Style.title}>非受控组件，type = square</Text>
-				<Slider type='square' defaultValue={30} />
-				<Text style={Style.title}>非受控组件，size = large</Text>
-				<Slider size='large' defaultValue={40} />
-				<Text style={Style.title}>非受控组件，size = small</Text>
-				<Slider size='small' defaultValue={50} />
-			</ScrollView>
-		)
-	}
+        <Text style={Style.title}>非受控组件，type = circle</Text>
+        <Slider type="circle" defaultValue={20} />
+        <Text style={Style.title}>非受控组件，type = square</Text>
+        <Slider type="square" defaultValue={30} />
+        <Text style={Style.title}>非受控组件，size = large</Text>
+        <Slider size="large" defaultValue={40} />
+        <Text style={Style.title}>非受控组件，size = small</Text>
+        <Slider size="small" defaultValue={50} />
+      </ScrollView>
+    );
+  }
 }
--- demo/app/src/ui/Tag/index.js
+++ demo/app/src/ui/Tag/index.js
@@ -1,106 +1,119 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, View, StyleSheet } from 'react-native'
-import { Style } from '../../../common'
-import { Tag, Icon } from 'fego-rn'
+import React, { Component } from 'react';
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
+import { Tag, Icon } from 'fego-rn';
+import { Style } from '../../../common';
+
 
 const style = StyleSheet.create({
-	container: {
-		flex: 1,
-		margin: 10,
-	},
+  container: {
+    flex: 1,
+    margin: 10,
+  },
 
-	header: {
-		marginLeft: 10,
-		marginTop: 10
-	},
+  header: {
+    marginLeft: 10,
+    marginTop: 10,
+  },
 
-	verticalContainer: {
-		flexDirection: 'column',
-		alignItems: 'flex-start'
-	},
+  verticalContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
 
-	horizonContainer: {
-		flexDirection: 'row',
-		alignItems: 'flex-end'
-	}
+  horizonContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
 
-})
+});
 
 export default class Page extends Component {
-	state = {
-		checked1: true,
-		selfTagSel: false,
-		closed1: false
-	}
-	constructor(props) {
-		super(props)
-		Tag.setBaseStyle(Tag, {
-			container: {
-				marginLeft: 10,
-				marginTop: 5,
-				marginBottom: 5
-			}
-		})
-	}
+  state = {
+    checked1: true,
+    selfTagSel: false,
+    closed1: false,
+  }
+  constructor(props) {
+    super(props);
+    Tag.setBaseStyle(Tag, {
+      container: {
+        marginLeft: 10,
+        marginTop: 5,
+        marginBottom: 5,
+      },
+    });
+  }
 
-	render() {
-		return (
-			<ScrollView style={Style.container}>
-				<View style={style.header}>
-					<Text>纵向布局</Text>
-				</View>
-				<View style={style.verticalContainer}>
-					<Tag text='使用 text props' />
-					<Tag text='text属性' >children属性，text属性失效</Tag>
-					<Tag >默认为非控组件</Tag>
-					<Tag checked={this.state.checked1} onChange={
-						checked => this.setState({ checked1: checked })
-					} >受控属性：checked</Tag>
+  render() {
+    return (
+      <ScrollView style={Style.container}>
+        <View style={style.header}>
+          <Text>纵向布局</Text>
+        </View>
+        <View style={style.verticalContainer}>
+          <Tag text="使用 text props" />
+          <Tag text="text属性" >children属性，text属性失效</Tag>
+          <Tag >默认为非控组件</Tag>
+          <Tag
+            checked={this.state.checked1}
+            onChange={
+              checked => this.setState({ checked1: checked })
+            }
+          >受控属性：checked
+          </Tag>
 
-					<Tag disabled>禁止的</Tag>
+          <Tag disabled>禁止的</Tag>
 
-					<Tag closed={this.state.closed1} onClose={
-						closed => this.setState({ closed1: closed })
-					}>受控属性：closed，开启关闭功能</Tag>
+          <Tag
+            closed={this.state.closed1}
+            onClose={
+              closed => this.setState({ closed1: closed })
+            }
+          >受控属性：closed，开启关闭功能
+          </Tag>
 
-					<Tag styles={{
-						checked: { borderColor: '#891232' }
-					}} checked={this.state.selfTagSel} onChange={
-						checked => this.setState({ selfTagSel: checked })
-					}>
-						<View style={{ flexDirection: 'row' }}>
-							<Icon style={{ color: this.state.selfTagSel ? '#891232' : '#bbb' }} name="calendar-check-o" />
-							<Text style={{ color: this.state.selfTagSel ? '#891232' : '#bbb' }}>自定义样式和内容</Text>
-							<Icon style={{ color: this.state.selfTagSel ? '#891232' : '#bbb' }} name="check" />
-						</View>
-					</Tag>
+          <Tag
+            styles={{
+              checked: { borderColor: '#891232' },
+            }}
+            checked={this.state.selfTagSel}
+            onChange={
+              checked => this.setState({ selfTagSel: checked })
+            }
+          >
+            <View style={{ flexDirection: 'row' }}>
+              <Icon style={{ color: this.state.selfTagSel ? '#891232' : '#bbb' }} name="calendar-check-o" />
+              <Text style={{ color: this.state.selfTagSel ? '#891232' : '#bbb' }}>自定义样式和内容</Text>
+              <Icon style={{ color: this.state.selfTagSel ? '#891232' : '#bbb' }} name="check" />
+            </View>
+          </Tag>
 
 
-					<Tag size="small">small</Tag>
-					<Tag size="large">large</Tag>
-					<Tag size="small" closed={false}>small</Tag>
-					<Tag size="large" closed={false}>large closed</Tag>
-					<Tag style={{width: 200}}>overflow controls how a children are measured and displayed. overflow: hidden causes views to be clipped while overflow: scroll causes views to be measured independently of their parents main axis</Tag>
+          <Tag size="small">small</Tag>
+          <Tag size="large">large</Tag>
+          <Tag size="small" closed={false}>small</Tag>
+          <Tag size="large" closed={false}>large closed</Tag>
+          <Tag style={{ width: 200 }}>overflow controls how a children are measured and displayed. overflow: hidden causes views to be clipped while overflow: scroll causes views to be measured independently of their parents main axis</Tag>
 
-				</View>
-				<View style={style.header}>
-					<Text>横向布局</Text>
-				</View>
-				<View style={style.horizonContainer}>
-					<Tag>默认tag</Tag>
-					<Tag disabled>disabled</Tag>
-					<Tag size="small">small</Tag>
-				</View>
-				<View style={style.header}>
-					<Text>等分</Text>
-				</View>
-				<View style={style.horizonContainer}>
-					<Tag style={{flex: 1}}>标签1</Tag>
-					<Tag style={{flex: 1}} disabled>disabled</Tag>
-					<Tag style={{flex: 2}} size="small">small</Tag>
-				</View>
-			</ScrollView>
-		)
-	}
+        </View>
+        <View style={style.header}>
+          <Text>横向布局</Text>
+        </View>
+        <View style={style.horizonContainer}>
+          <Tag>默认tag</Tag>
+          <Tag disabled>disabled</Tag>
+          <Tag size="small">small</Tag>
+        </View>
+        <View style={style.header}>
+          <Text>等分</Text>
+        </View>
+        <View style={style.horizonContainer}>
+          <Tag style={{ flex: 1 }}>标签1</Tag>
+          <Tag style={{ flex: 1 }} disabled>disabled</Tag>
+          <Tag style={{ flex: 2 }} size="small">small</Tag>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
