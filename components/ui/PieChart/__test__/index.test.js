98cbaf0919b1e65af64e15c417ca9b254ea60ecf
荆博 <525456448@qq.com>
2018-02-27 14:34:17 +0800

    MOD：格式

--- components/ui/PieChart/__test__/index.test.js
+++ components/ui/PieChart/__test__/index.test.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import Renderer from 'react-test-renderer'
-import { Text } from 'react-native'
-import { shallow } from 'enzyme'
-import PieChart from '../index'
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import { Text } from 'react-native';
+import PieChart from '../index';
 
 describe('PieChart Tests', () => {
-	let Comp;
-	beforeEach(() => {
-		Comp = <PieChart
-			percentArray={[0.2, 0.1, 0.4, 0.3]}
-			colorArray={['red', 'yellow','blue','green']}
-			outerRadius={40}
-			innerRadius={25}
-			duration={1000}
-		/>
-	})
-	it('renders correctly', () => {
-		const tree = Renderer.create(<Text>PieChart</Text>).toJSON();
-		expect(tree).toMatchSnapshot();
-	});
-
-	// it('other', () => {
-	// 	const wrapper = shallow(Comp);
-	// 	expect(wrapper.instance().props.innerRadius).toBe(25);
-	// });
-})
+  let Comp;
+  beforeEach(() => {
+    Comp = (
+      <PieChart
+        percentArray={[0.2, 0.1, 0.4, 0.3]}
+        colorArray={['red', 'yellow', 'blue', 'green']}
+        outerRadius={40}
+        innerRadius={25}
+        duration={1000}
+      />
+    );
+  });
+  if (!Comp) { return; }
+  it('renders correctly', () => {
+    const tree = Renderer.create(<Text>PieChart</Text>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  // it('other', () => {
+  //   const wrapper = shallow(Comp);
+  //   expect(wrapper.instance().props.innerRadius).toBe(25);
+  // });
+});
--- components/ui/RefreshView/RefreshView.js
+++ components/ui/RefreshView/RefreshView.js
@@ -1,6 +1,6 @@
 
 
-import React, { Component } from 'react';
+import React from 'react';
 import { ScrollView } from 'react-native';
 
 import RefreshBaseView from './RefreshBaseView';
