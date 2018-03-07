206699172d709eede221183b607e732b65c4042e
天天 <xiangbo-99@163.com>
2018-03-07 11:55:08 +0800

    规则修改

--- demo/app/src/ui/Badge/index.js
+++ demo/app/src/ui/Badge/index.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import { Badge } from 'fego-rn';
 
-export default class Page extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, paddingHorizontal: 10 }}>
-        <View>
-          <Text>badge</Text>
-          <Badge />
-        </View>
-      </View>
-    );
-  }
-}
+export default () => (
+  <View style={{ flex: 1, paddingHorizontal: 10 }}>
+    <View>
+      <Text>badge</Text>
+      <Badge />
+    </View>
+  </View>
+);
