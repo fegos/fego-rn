4e248fae5f48fdbaf312e7ea66b343e01b68d325
天天 <xiangbo-99@163.com>
2018-04-18 19:16:53 +0800

    Ignore some warning

--- demo/app/index.js
+++ demo/app/index.js
@@ -4,6 +4,15 @@ import { AppContainer } from 'fego-rn';
 import StackNav from './config/StackNav';
 import Font from './config/font';
 
+// 忽略warning
+if (__DEV__) {
+  console.ignoredYellowBox = [
+    'Remote debugger is in a background tab which may cause apps to perform slowly. Fix this by foregrounding the tab (or opening it in a separate window).',
+    'Debugger and device times have drifted by more than 60s. Please correct this by running adb shell "date `date +%m%d%H%M%Y.%S`" on your debugger machine.',
+    'Warning: isMounted(...) is deprecated',
+  ];
+}
+
 export default {
   init: () => {
     Font.init();
